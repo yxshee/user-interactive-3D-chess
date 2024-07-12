658a934dcea6c949a3389bf028759a3b9553ee37
yash <84263676+yxshee@users.noreply.github.com>
2024-07-12 14:46:14 +0530

    Add files via upload

---
+++ 3d-chess-opengl-master/Model.h
@@ -0,0 +1,23 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <vector>
+#include <string>
+
+class Model
+{
+private:
+    //Attribute
+    std::vector<float> position_texture_normal;
+    int sz;
+    //Function
+    std::vector<std::string> splitString(std::string str, char delimiter);
+    float getFloat(std::string input);
+    int getInt(std::string input);
+public:
+    Model(std::string objFilePath);
+    ~Model();
+    void Draw();
+};
+
+#endif
